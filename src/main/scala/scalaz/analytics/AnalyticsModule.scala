c02972e72bd1e51920e47dd255674ca9525cef9d
Cameron Joannidis <cameron.joannidis@gmail.com>
2018-08-18 22:16:06 +1000

    Remove verticalMultiline from scalafmt

--- src/main/scala/scalaz/analytics/AnalyticsModule.scala
+++ src/main/scala/scalaz/analytics/AnalyticsModule.scala
@@ -2,35 +2,34 @@ package scalaz.analytics
 
 import scalaz.zio.IO
 
-
 /**
-  * An analytics module defined the abstract components required to build a reified program description.
-  */
+ * An analytics module defined the abstract components required to build a reified program description.
+ */
 trait AnalyticsModule {
 
   /**
-    * An abstract DataStream.
-    */
+   * An abstract DataStream.
+   */
   type DataStream[_]
 
   /**
-    * The set of types that are representable in this module.
-    */
+   * The set of types that are representable in this module.
+   */
   type Type[_]
 
   /**
-    * The subset of types which can be viewed as NumericType.
-    */
+   * The subset of types which can be viewed as NumericType.
+   */
   type NumericType[A]
 
   /**
-    * A type that represents an Unknown Schema for some Data.
-    */
+   * A type that represents an Unknown Schema for some Data.
+   */
   type Unknown
 
   /**
-    * Proofs that the various types are [[Type]]s or [[NumericType]]s.
-    */
+   * Proofs that the various types are [[Type]]s or [[NumericType]]s.
+   */
   implicit val unknown: Type[Unknown]
   implicit val intNumeric: NumericType[Int]
   implicit val longNumeric: NumericType[Long]
@@ -39,33 +38,43 @@ trait AnalyticsModule {
   implicit def tuple2Type[A: Type, B: Type]: Type[(A, B)]
 
   /**
-    * Allows us to prove that anytime we have a NumericType we also have a Type
-    */
+   * Allows us to prove that anytime we have a NumericType we also have a Type
+   */
   implicit def numericType[A: NumericType]: Type[A]
 
   /**
-    * An Arrow between A and B.
-    * Arrows allow us to define abstract pipelines in scalaz-analytics.
-    */
+   * An Arrow between A and B.
+   * Arrows allow us to define abstract pipelines in scalaz-analytics.
+   */
   type =>:[-A, +B]
 
   /**
-    * Syntax a user can use to build their pipelines
-    */
+   * Syntax a user can use to build their pipelines
+   */
   implicit class FunctionSyntax[A, B](self: A =>: B) {
-    def andThen[C: Type](that: B =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: C = stdLib.compose(that, self)
-    def >>>[C: Type](that: B =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: C = andThen(that)
 
-    def compose[C: Type](that: C =>: A)(implicit typeA: Type[A], typeB: Type[B]): C =>: B = stdLib.compose(self, that)
-    def <<<[C: Type](that: C =>: A)(implicit typeA: Type[A], typeB: Type[B]): C =>: B = compose(that)
+    def andThen[C: Type](that: B =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: C =
+      stdLib.compose(that, self)
 
-    def combine[C: Type](that: A =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: (B, C) = stdLib.fanOut(self, that)
-    def &&&[C: Type](that: A =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: (B, C) = stdLib.fanOut(self, that)
+    def >>> [C: Type](that: B =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: C =
+      andThen(that)
+
+    def compose[C: Type](that: C =>: A)(implicit typeA: Type[A], typeB: Type[B]): C =>: B =
+      stdLib.compose(self, that)
+
+    def <<< [C: Type](that: C =>: A)(implicit typeA: Type[A], typeB: Type[B]): C =>: B =
+      compose(that)
+
+    def combine[C: Type](that: A =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: (B, C) =
+      stdLib.fanOut(self, that)
+
+    def &&& [C: Type](that: A =>: C)(implicit typeA: Type[A], typeB: Type[B]): A =>: (B, C) =
+      stdLib.fanOut(self, that)
   }
 
   /**
-    * The Operations supported by the core DataStream abstraction in scalaz-analytics.
-    */
+   * The Operations supported by the core DataStream abstraction in scalaz-analytics.
+   */
   trait SetOperations {
     def empty[A: Type]: DataStream[A]
 
@@ -85,8 +94,8 @@ trait AnalyticsModule {
   }
 
   /**
-    * The standard library of scalaz-analytics.
-    */
+   * The standard library of scalaz-analytics.
+   */
   trait StandardLibrary {
     def id[A: Type]: A =>: A
 
@@ -103,7 +112,7 @@ trait AnalyticsModule {
 
     def fanOut[A: Type, B: Type, C: Type](fst: A =>: B, snd: A =>: C): A =>: (B, C)
 
-    def split[A: Type, B: Type, C: Type, D: Type](f: A =>: B, g: C =>: D): (A,  C) =>: (B, D)
+    def split[A: Type, B: Type, C: Type, D: Type](f: A =>: B, g: C =>: D): (A, C) =>: (B, D)
 
     def product[A: Type, B: Type](fab: A =>: B): (A, A) =>: (B, B)
 
@@ -118,11 +127,12 @@ trait AnalyticsModule {
   }
 
   /**
-    * A DSL for numeric operations
-    */
+   * A DSL for numeric operations
+   */
   implicit class NumericSyntax[A, B](val l: A =>: B) {
+
     def * (r: A =>: B)(implicit A: Type[A], B: NumericType[B]): A =>: B =
-      (l combine r) andThen stdLib.mult
+      (l.combine(r)).andThen(stdLib.mult)
 
     def + (r: A =>: B)(implicit A: Type[A], B: NumericType[B]): A =>: B =
       (l &&& r) >>> stdLib.sum
@@ -135,9 +145,10 @@ trait AnalyticsModule {
   }
 
   /**
-    * A DSL for building the DataStream data structure in a manner familiar to libraries like Spark/Flink etc
-    */
+   * A DSL for building the DataStream data structure in a manner familiar to libraries like Spark/Flink etc
+   */
   implicit class DataStreamSyntax[A](d: DataStream[A])(implicit A: Type[A]) {
+
     def map[B: Type](f: (A =>: A) => (A =>: B)): DataStream[B] =
       setOps.map(d)(f(stdLib.id))
 
@@ -148,19 +159,18 @@ trait AnalyticsModule {
   val setOps: SetOperations
   val stdLib: StandardLibrary
 
-
   /**
-    * Described a transformation from some Unknown Column type to a concrete Type
-    */
+   * Described a transformation from some Unknown Column type to a concrete Type
+   */
   def column[A: Type](str: String): Unknown =>: A = stdLib.column(str)(implicitly[Type[A]])
 
   /**
-    * Loads a [[DataStream]] without type information
-    */
+   * Loads a [[DataStream]] without type information
+   */
   def load(path: String): DataStream[Unknown]
 
   /**
-    * Execute the [[DataStream]]
-    */
+   * Execute the [[DataStream]]
+   */
   def run[A: Type](d: DataStream[A]): IO[Error, Seq[A]]
 }
