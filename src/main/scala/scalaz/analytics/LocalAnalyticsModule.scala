f2e480c21925f06f17c3d2f60dcc68f2a33de642
Jed Wesley-Smith <jed@wesleysmith.io>
2018-08-18 17:56:18 +1000

    cleanups, changed the names for a little clarity

--- src/main/scala/scalaz/analytics/LocalAnalyticsModule.scala
+++ src/main/scala/scalaz/analytics/LocalAnalyticsModule.scala
@@ -7,12 +7,12 @@ import scalaz.zio.IO
   */
 trait LocalAnalyticsModule extends AnalyticsModule {
   override type DataStream[A] = LocalDataStream
-  override type Type[A] = TypeTypeclass[A]
+  override type Type[A] = LocalType[A]
   override type Unknown = UnknownType
   type UnknownType
   override type =>:[-A, +B] = RowFunction
 
-  private object Nums {
+  private object LocalNum {
     def apply[A: Type]: Num[A] =
       new Num[A] {
         override val typeOf = Type[A]
@@ -24,67 +24,68 @@ trait LocalAnalyticsModule extends AnalyticsModule {
   }
 
   override implicit val unknown: Type[Unknown] = new Type[Unknown] {
-    override def reified: ReifiedType = ReifiedType.Unknown
+    override def reified: Reified = Reified.Unknown
   }
-  override implicit val intType: Type[scala.Int] = ReifiedType(ReifiedType.Int)
-  override implicit val intNumeric: Num[scala.Int] = Nums[Int]
+  override implicit val intType: Type[scala.Int] = LocalType(Reified.Int)
+  override implicit val intNumeric: Num[scala.Int] = LocalNum[Int]
 
-  override implicit val longType: Type[scala.Long] = ReifiedType(ReifiedType.Long)
-  override implicit val longNumeric: Num[scala.Long] = Nums[scala.Long]
+  override implicit val longType: Type[scala.Long] = LocalType(Reified.Long)
+  override implicit val longNumeric: Num[scala.Long] = LocalNum[scala.Long]
 
-  override implicit val floatType: Type[scala.Float] = ReifiedType(ReifiedType.Float)
-  override implicit val floatNumeric: Num[scala.Float] =  Nums[scala.Float]
+  override implicit val floatType: Type[scala.Float] = LocalType(Reified.Float)
+  override implicit val floatNumeric: Num[scala.Float] =  LocalNum[scala.Float]
 
-  override implicit val doubleType: Type[scala.Double] = ReifiedType(ReifiedType.Double)
-  override implicit val doubleNumeric: Num[scala.Double] =  Nums[scala.Double]
+  override implicit val doubleType: Type[scala.Double] = LocalType(Reified.Double)
+  override implicit val doubleNumeric: Num[scala.Double] =  LocalNum[scala.Double]
 
   override implicit def tuple2Type[A: Type, B: Type]: Type[(A, B)] = new Type[(A, B)] {
-    override def reified: ReifiedType = ReifiedType.Tuple2(TypeTypeclass.typeof[A], TypeTypeclass.typeof[B])
+    override def reified: Reified = Reified.Tuple2(LocalType.typeof[A], LocalType.typeof[B])
   }
 
 
   /**
-    * A typeclass that produces a ReifiedType
+    * A typeclass that produces a Reified
     */
-  sealed trait TypeTypeclass[A] {
-    def reified: ReifiedType
+  sealed trait LocalType[A] {
+    def reified: Reified
   }
-  object TypeTypeclass {
-    def typeof[A](implicit ev: TypeTypeclass[A]): ReifiedType = ev.reified
+  object LocalType {
+    def typeof[A](implicit ev: LocalType[A]): Reified = ev.reified
+
+    private[LocalAnalyticsModule] def apply[A](r: Reified): Type[A] =
+      new Type[A] {
+        override def reified: Reified = r
+      }
   }
 
   /**
     * The set of reified types.
     * These represent all the Types that scalaz-analytics works with
     */
-  sealed trait ReifiedType
-  object ReifiedType {
-    private[LocalAnalyticsModule] def apply[A](r: ReifiedType): Type[A] =
-      new Type[A] {
-        override def reified: ReifiedType = r
-      }
+  sealed trait Reified
+  object Reified {
 
-    case object Int extends ReifiedType
-    case object Long extends ReifiedType
-    case object Float extends ReifiedType
-    case object Double extends ReifiedType
-    case object String extends ReifiedType
-    case object Decimal extends ReifiedType
-    case object Unknown extends ReifiedType
-    case class Tuple2(a: ReifiedType, b: ReifiedType) extends ReifiedType
+    case object Int extends Reified
+    case object Long extends Reified
+    case object Float extends Reified
+    case object Double extends Reified
+    case object String extends Reified
+    case object Decimal extends Reified
+    case object Unknown extends Reified
+    case class Tuple2(a: Reified, b: Reified) extends Reified
   }
-  
+
   /**
     * A Marker for types that are numeric
     */
-  trait NumericReifiedType extends ReifiedType
+  trait NumericReifiedType extends Reified
 
   /**
     * A reified DataStream program
     */
   sealed trait LocalDataStream
   object LocalDataStream {
-    case class Empty(rType: ReifiedType) extends LocalDataStream
+    case class Empty(rType: Reified) extends LocalDataStream
     case class Union(a: LocalDataStream, b: LocalDataStream) extends LocalDataStream
     case class Intersect(a: LocalDataStream, b: LocalDataStream) extends LocalDataStream
     case class Except(a: LocalDataStream, b: LocalDataStream) extends LocalDataStream
@@ -95,7 +96,7 @@ trait LocalAnalyticsModule extends AnalyticsModule {
   }
 
   override val setOps: SetOperations = new SetOperations {
-    override def empty[A: Type]: LocalDataStream = LocalDataStream.Empty(TypeTypeclass.typeof[A])
+    override def empty[A: Type]: LocalDataStream = LocalDataStream.Empty(LocalType.typeof[A])
 
     override def union[A: Type](l: LocalDataStream, r: LocalDataStream): LocalDataStream = LocalDataStream.Union(l, r)
 
@@ -119,16 +120,16 @@ trait LocalAnalyticsModule extends AnalyticsModule {
     */
   sealed trait RowFunction
   object RowFunction {
-    case class Id(reifiedType: ReifiedType) extends RowFunction
+    case class Id(reifiedType: Reified) extends RowFunction
     case class Compose(left: RowFunction, right: RowFunction) extends RowFunction
-    case class Mult[A](typ: ReifiedType) extends RowFunction
-    case class Sum(typ: ReifiedType) extends RowFunction
-    case class Diff(typ: ReifiedType) extends RowFunction
-    case class Mod[A](typ: ReifiedType) extends RowFunction
+    case class Mult[A](typ: Reified) extends RowFunction
+    case class Sum(typ: Reified) extends RowFunction
+    case class Diff(typ: Reified) extends RowFunction
+    case class Mod[A](typ: Reified) extends RowFunction
     case class FanOut(fst: RowFunction, snd: RowFunction) extends RowFunction
     case class Split(f: RowFunction, g: RowFunction) extends RowFunction
     case class Product(fab: RowFunction) extends RowFunction
-    case class Column(colName: String, rType: ReifiedType) extends RowFunction
+    case class Column(colName: String, rType: Reified) extends RowFunction
 
     // constants
     case class IntLiteral(value: Int) extends RowFunction
@@ -140,7 +141,7 @@ trait LocalAnalyticsModule extends AnalyticsModule {
   }
 
   override val stdLib: StandardLibrary = new StandardLibrary {
-    override def id[A: Type]: A =>: A = RowFunction.Id(TypeTypeclass.typeof[A])
+    override def id[A: Type]: A =>: A = RowFunction.Id(LocalType.typeof[A])
 
     override def compose[A, B, C](f: B =>: C, g: A =>: B): A =>: C = RowFunction.Compose(f, g)
 
@@ -160,7 +161,7 @@ trait LocalAnalyticsModule extends AnalyticsModule {
     override def string[A](v: scala.Predef.String): A =>: String = RowFunction.StringLiteral(v)
 
     // todo this needs more thought
-    override def column[A: Type](str: String): Unknown =>: A = RowFunction.Column(str, TypeTypeclass.typeof[A])
+    override def column[A: Type](str: String): Unknown =>: A = RowFunction.Column(str, LocalType.typeof[A])
   }
 
   def load(path: String): DataStream[Unknown] = ???
